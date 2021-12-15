53077e8c2c40bc310460c73aa59f69752520c660
Ayudya <Tabitha.Ayudya@team.telstra.com>
2021-12-15 14:40:22 +1100

    added frontend stack

---
+++ stacks/FrontendStack.js
@@ -0,0 +1,25 @@
+import * as sst from "@serverless-stack/resources";
+
+export default class FrontendStack extends sst.Stack {
+  constructor(scope, id, props) {
+    super(scope, id, props);
+
+    const { api, auth, bucket } = props;
+
+
+    // Define our React app
+    const site = new sst.ReactStaticSite(this, "ReactSite", {
+      path: "frontend",
+      // Pass in our environment variables
+      environment: {
+        REACT_APP_API_URL: api.url,
+        REACT_APP_REGION: scope.region,
+      },
+    });
+
+    // Show the url in the output
+    this.addOutputs({
+      SiteUrl: site.url,
+    });
+  }
+}
--- stacks/index.js
+++ stacks/index.js
@@ -1,10 +1,17 @@
 import StorageStack from "./StorageStack";
 import ApiStack from "./ApiStack";
+import FrontendStack from "./FrontendStack";
 
 export default function main(app) {
   const storageStack = new StorageStack(app, "storageNew3");
 
-  new ApiStack(app, "apiNew3", {
+  const apiStack = new ApiStack(app, "apiNew3", {
     table: storageStack.table,
   });
+
+  new FrontendStack(app, "frontend", {
+    api: apiStack.api,
+    bucket: storageStack.bucket,
+  });
+    
 }
