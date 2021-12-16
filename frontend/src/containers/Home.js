db439a48b785d3d3ea88ee5bf29a2bec96b16b7a
Ratnayake Mudiyanselage <Vilanya.RatnayakeMudiyanselage@team.telstra.com>
2021-12-16 15:18:33 +1100

    Added Navbar and homepage route

---
+++ frontend/src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>Brownie</h1>
+        <p className="text-muted">All you need for your next team meeting</p>
+      </div>
+    </div>
+  );
+}
