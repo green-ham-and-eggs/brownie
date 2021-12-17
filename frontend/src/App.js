310ea0807b3320e4bbc6b33a0c532fbf4bfd4d77
Ratnayake Mudiyanselage <Vilanya.RatnayakeMudiyanselage@team.telstra.com>
2021-12-17 11:32:54 +1100

    New homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,18 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import "./App.css";
+import Routes from "./Routes";
 
 function App() {
   return (
     <div className="App container py-3">
       <Navbar collapseOnSelect bg="light" expand="md" className="mb-3">
         <Navbar.Brand className="font-weight-bold text-muted">
-          Scratch
+          Brownie
         </Navbar.Brand>
         <Navbar.Toggle />
       </Navbar>
+      <Routes />
     </div>
   );
 }
